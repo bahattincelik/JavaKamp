7f978567f1fded121f3b7d30126fe9478691493e
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-01 01:49:56 +0300

    java kamp

--- src/videoExamplesClasses02/Main.java
+++ src/videoExamplesClasses02/Main.java
@@ -3,16 +3,20 @@ package videoExamplesClasses02;
 public class Main {
 
 	public static void main(String[] args) {
+		Product product2 = new Product(1,"Laptop","Asus",3000,2,"Siyah");
 		Product product = new Product();
-		product.name="Laptop";
-		product.id=1;
-		product.description="Asus";
-		product.price=5000;
-		product.stockAmount=3;
+		product.setName("Laptop");
+		product.setId(1);
+		
+		product.setDescription("Asus");
+		product.setPrice(5000);
+		product.setStockAmount(3);
+		product.setRenk("Siyah");
 		
 		
 		ProductManager productManager = new ProductManager();
 		productManager.add(product);
+		System.out.println(product.getKod());
 	}
 
 }
--- src/videoExamplesClasses02/ProductManager.java
+++ src/videoExamplesClasses02/ProductManager.java
@@ -3,7 +3,7 @@ package videoExamplesClasses02;
 public class ProductManager {
 	public void add(Product product) {
 		//JDBC
-		System.out.println("Urun Eklendi " + product.name);
+		System.out.println("Urun Eklendi " + product.getName());
 	}
 	
 	public void add2(int id, String name,String description, int stockAmount,double price) {
