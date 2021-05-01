ff5596988d638cf1da8daa40d70ae1d59217ba25
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-05-01 22:59:55 +0300

    java

--- src/homework03/InstructorManager.java
+++ src/homework03/InstructorManager.java
@@ -1,5 +1,12 @@
 package homework03;
 
 public class InstructorManager extends UserManager {
+
+	@Override
+	public void showInfos(User user) {
+		// TODO Auto-generated method stub
+		super.showInfos(user);
+		System.out.println("Egitmen eklendi");
+	}
 	
 }
--- src/homework03/Main.java
+++ src/homework03/Main.java
@@ -4,10 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		User student = new Student(1, "Bahattin", "Celik","bahattinclk@gmail.com");
-		UserManager userManager = new StudentManager();
-		userManager.showInfos(student);
+		UserManager user1 = new StudentManager();
+		user1.showInfos(student);
 		
+		System.out.println();
 		
+		User instructor = new Instructor(2, "Bahattin", "Celik",5000);
+		UserManager user2 = new InstructorManager();
+		user2.showInfos(instructor);
 	}
 
 }
